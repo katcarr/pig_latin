e83603ea918a47cea25656f9ce5ea20013fa69ee
Christopher Joy-Webb and Kathryn Carr <chris@joywebb.com>
2015-02-09 16:51:07 -0800

    add translator for words starting with multiple consonants

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,19 @@
 var pigLatin = function(word) {
   var vowels = ["a", "e", "i", "o", "u"];
-
-  var begin_vowel = false;
-
-  vowels.forEach(function(vowel) {
-    if (word[0] === vowel) {
-      begin_vowel = true;
+  var consonants = ["b","c","d", "f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x", "y", "z"]
+  var vowel_not_found = true;
+  var new_end = "";
+  while(vowel_not_found){
+    for (var i = 0; i < consonants.length; i += 1) {
+      if(word[0] === consonants[i]){
+        new_end = new_end + word[0];
+        word = word.substr(1);
+        vowel_not_found = true;
+        break;
+      } else{
+        vowel_not_found = false;
+      }
     }
-  });
-
-  if (begin_vowel) {
-    word = word + "ay";
-  } else {
-    var new_end = word.slice(0,1) + "ay";
-    word = word.substr(1) + new_end;
   }
-
-  return word;
-
+  return word + new_end + "ay";
 };
